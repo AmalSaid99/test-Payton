9b3f8c2fa77290192e080677ecf99b25cd408d1c
AmalSaid99 <As1511629@gmail.com>
2023-11-14 13:55:40 +0400

    test2

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello")
+T= 100 #total
+a= 5
+b= T/a
+print (b)
+black= b/2
+white= black-1
+print (black)
+print (white)
+g=white/(black-white)
+print(g)
+
---
+++ test2.py
@@ -0,0 +1,3 @@
+name= input("enter your name: ")
+age= int(input("enter a age: "))
+print("%-5s" %(name), "%5d" %(age))
