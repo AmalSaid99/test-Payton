1338259efa898f2a3633f93cccd390f7fdb8347f
AmalSaid99 <As1511629@gmail.com>
2023-11-16 15:25:59 +0400

    greate number

---
+++ greatNO.py
@@ -0,0 +1,16 @@
+a= int(input("enter number1: "))
+b= int(input("enter number2: "))
+c= int(input("enter number3: "))
+
+if(a>=b and a>=c):
+    print(str(a)+" is the largest")
+elif(b>=a and b>=c):
+    print(str(b)+ "is te largest")
+else:
+    print("")
+
+
+
+
+
+
