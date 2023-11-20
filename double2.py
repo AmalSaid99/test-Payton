e3660ecc25f65bd8d7a97dee3066968bbec01ce0
AmalSaid99 <As1511629@gmail.com>
2023-11-20 16:22:05 +0400

    double2

---
+++ double2.py
@@ -0,0 +1,18 @@
+n= 4
+sum1 = 0
+for i in range(1, n+1):
+   result= "2"*i
+   sum1 +=int(result)
+print(sum1)
+        
+#for num in range (1,5):
+   # for i in range(1, num):
+       # if(num+i==0):
+         #   sum1+=i
+             
+   # if(sum1==num):
+     #  print(num)
+   # sum1=0
+
+        
+   
