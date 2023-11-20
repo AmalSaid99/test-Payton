d2016ca9da20d9ab3d152035d87d705a730a1788
AmalSaid99 <As1511629@gmail.com>
2023-11-20 15:19:11 +0400

    assignment1Q2

--- assignment1(q2).py
+++ assignment1(q2).py
@@ -1,15 +1,16 @@
-int_tuituon= 1000
+int_tuituon= 10000
 rate= 0.05
 years=10
 i=0
 curr_year= 1
 while(curr_year <=  years):
+    int_tuituon=int_tuituon*(1+rate)
     print("Tuition in year: "+str(curr_year)+": "+ "$"+str(int_tuituon))
     
     if(curr_year <= 4):
        i+= int_tuituon
        
-       curr_year +=1
+    curr_year +=1
        
        
 print("total cost for 4 years after 10 year: $"+str(i))
