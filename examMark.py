43c0f4ecf03ab3c1eb988fcd393cdab05a39b708
AmalSaid99 <As1511629@gmail.com>
2023-11-20 16:23:20 +0400

    exam marks

---
+++ examMark.py
@@ -0,0 +1,26 @@
+mark=0
+answer= "abaacb"
+user_answer=input("enter your answer: ")
+for i in range(len(answer)):
+    if(user_answer[i] == answer[i]):
+        mark+=1
+print(mark, "out of", len(answer))
+
+
+
+#if(a[0]=="a"):
+    #print("
+    #elif(a[1]=="b"):
+    #q2= input("Enter s answer: ")
+    #(a[2]=="a")
+    #q3= input("Enter t answer: ")
+    #(a[3]=="a")
+    #q4= input("Enter f answer: ")
+   #(a[4]=="c")
+     #(a[5]=="b")
+    
+#q2= input("Enter the answer: ")
+
+
+
+
