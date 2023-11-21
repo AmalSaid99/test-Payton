0c2ac079589cfd4e5aa17182f511699eceaf8f38
AmalSaid99 <As1511629@gmail.com>
2023-11-21 15:25:34 +0400

    list4

---
+++ append.py
@@ -0,0 +1,26 @@
+#append(add new element in last position)
+s= [2,5,5,1,9]
+s.append(7)
+print(s)
+
+print("**********")
+
+friends= []
+friends.append("iii")
+friends.append("rrr")
+
+print(friends)
+
+# insert(add new element in specific position)
+print("**********")
+s= [2,5,5,1,9]
+s.insert(0, 77)
+print(s)
+
+#index
+print("**********")
+s= [2,5,5,1,9]
+print(s[3])
+print(s.index(2))
+print(s)
+
