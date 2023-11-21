a2d4ec9437e7c3c6abbc0477ee912513ee0d230d
AmalSaid99 <As1511629@gmail.com>
2023-11-21 15:28:52 +0400

    append2

---
+++ append2.py
@@ -0,0 +1,12 @@
+number=int(input("enter a number: "))
+result=[]
+
+while number !="Q":
+     i=result.append(number)
+     number=input("enter a number: ")
+
+else:
+    print("Q")
+    print(result)
+    
+    
