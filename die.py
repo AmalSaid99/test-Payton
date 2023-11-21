7272efc4d6850479940bcf92c56f9869e6949c26
AmalSaid99 <As1511629@gmail.com>
2023-11-21 15:31:44 +0400

    rolldie

---
+++ die.py
@@ -0,0 +1,22 @@
+d=["123456"]
+def main():
+    print(countInput(sides), countOutput(counter))
+def countInput(6):
+    side=["123456"]
+    return 
+def countOutput(counter):
+    
+    
+    
+"""def main():
+    print(func1(2),func2(1))
+def func1(x):
+    num = 5
+    return x*num
+def func2(x):
+    i=2
+    y=3
+    return i*x+y
+main()"""
+
+    
