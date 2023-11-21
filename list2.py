1796ae1b297e8075db3047941f374bac347d7167
AmalSaid99 <As1511629@gmail.com>
2023-11-21 15:22:48 +0400

    list2

---
+++ list2.py
@@ -0,0 +1,14 @@
+list_=[7,8,9,3]
+
+for i in range(len(list_)):
+    print(i)
+    
+print("***********")
+list_=[7,8,9,3]
+
+for i in range(len(list_)):
+    print(list_[i])
+    
+print("***********")
+for i in[2,4]:
+    print(i)
