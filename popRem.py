5a9d3aef107e940437c01fe2df17c7fab84a05a8
AmalSaid99 <As1511629@gmail.com>
2023-11-21 15:27:30 +0400

    popRemove

---
+++ popRem.py
@@ -0,0 +1,26 @@
+#pop(delete the last elemet)
+s= [-2,5,-5,1,-7]
+old_value= s.pop(-1)
+print(old_value)
+print(s)
+
+print("**********")
+
+#remove(delete element from specific psition)
+s= [-2,5,-5,1,-7]
+old_value= s.remove(-5)
+print(old_value)
+print(s)
+
+print("**********")
+
+
+ls=[0]*4
+print(ls)
+
+#sort(orser number)
+print("**********")
+
+values=[1,5,4,3]
+values.sort()
+print(values)
